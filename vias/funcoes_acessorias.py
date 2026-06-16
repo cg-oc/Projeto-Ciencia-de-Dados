a5e300395ea83b0e9a794e1956f07926079d0f35
Carlos G. de Oliveira Campos <carlos2610010@ilum.cnpem.br>
2026-06-16 17:06:49 -0300

    Removidas redundâncias de inputs e criada nova função que gera png representando o grafo que está sendo analisado a cada momento. Essa função é chamada a cada mudança do grafo ou quando é detectado que o arquipélago não é simples.

--- vias/funcoes_acessorias.py
+++ vias/funcoes_acessorias.py
@@ -3,15 +3,14 @@ import time
 from networkx import Graph, draw
 
 class Arquipelago:
-    def __init__(self, n_ilhas: int, n_conexoes: int, anos: int):
-        self.n_ilhas: int = n_ilhas
+    def __init__(self,n_conexoes: int, anos: int):
         self.n_conexoes: int = n_conexoes
         self.anos: int = anos
         self.rodovias: set[tuple] = set() # preciso mudar para set[tuple]
-        self.grafo: dict[set] = {}
+        self.grafo: dict[set, int] = {}
         self.n_loops: int = 0
     
-    def pega_conexoes(self):
+    def pega_conexoes(self) -> None:
         """
         Pede ao usuário as conexões do arquipélago e as armazena no objeto `Arquipelago`. Rodovias são salvas
         em set exclusivo `self.rodovias` para serem verificadas uma a uma posteriormente.
@@ -25,6 +24,7 @@ class Arquipelago:
 
             else: # conexao = 2 = rodovia
                 self.rodovias.add((v1, v2))
+        self.mostra_grafo("Somente hidrovias")
     
     def pega_ponta(self, visitados: set) -> int | None:
         """
@@ -74,20 +74,24 @@ class Arquipelago:
         else:
             self.grafo[v2]: set = {v1}
 
-        mostra_grafo(self.grafo)
+    def remove_conexao(self, v1: int, v2: int) -> None:
+        self.grafo[v1].remove(v2)
+        self.grafo[v2].remove(v1)
 
     def checa_loop_de_rodovias(self) -> bool:
         for rodovia in self.rodovias:
             self.adiciona_conexao(*rodovia)
-            mostra_grafo(self.grafo)
+            self.mostra_grafo(f"Adicionada a rodovia {rodovia}")
 
             if not self.tem_loop():
                 continue # rodovia é mantida
             
             self.n_loops += 1 #tem loop
             if self.n_loops > self.anos: #verifica se nao cruzou o limite
+                self.mostra_grafo(f"Loops de rodovia demais! Arquipélago não é simples", cor='red')
                 return False #arquipelago não era simples
-            self.rodovias.remove(rodovia) # passa a desconsiderar rodovia que foi "destruída"
+            self.remove_conexao(*rodovia)
+            self.mostra_grafo(f"Removida a rodovia {rodovia}")
         return True
 
     def era_simples(self) -> bool:
@@ -98,14 +102,38 @@ class Arquipelago:
         """
         # cehcando primeiro somente com hidrovias
         if self.tem_loop():
+            self.mostra_grafo(f"Loops de rodovia demais! Arquipélago não é simples", cor='red')            
             return False
         else:
             # agora checa uma rodovia por vez
             return self.checa_loop_de_rodovias()
+    
+    def mostra_grafo(self, mensagem: str = "", cor: str = "blue") -> None:
+        """
+        Gera .png representando o grafo do arquipélago com todas as hidrovias e as 
+        rodovias adicionadas até então.
+        """
+        g = Graph(self.grafo)
+        draw(g, with_labels=True)
+        if mensagem:
+            plt.figtext(0.5, 0.01, mensagem, wrap=True, horizontalalignment='center', fontsize=12, color=cor)
+        plt.title(f"Anos: {self.anos}           Loops de rodovia: {self.n_loops}")
+        plt.savefig("meu_grafo.png", format="PNG", dpi=300, bbox_inches="tight")
+        plt.close()
+        # time.sleep(5)
+        input("ENTER para continuar")
 
-def mostra_grafo(grafo: dict) -> None:
-    g = Graph(grafo)
-    draw(g, with_labels=True)    
-    plt.savefig("meu_grafo.png", format="PNG", dpi=300, bbox_inches="tight")
-    plt.close()
-    time.sleep(3)
+
+# def mostra_grafo(grafo: dict, mensagem: str = "") -> None:
+#     g = Graph(grafo)
+#     draw(g, with_labels=True)
+    
+#     # Adiciona um texto na parte inferior centralizada
+#     # x=0.5 (centro horizontal), y=0.01 (bem próximo à borda inferior)
+#     if mensagem:
+#         plt.figtext(0.5, 0.01, mensagem, wrap=True, horizontalalignment='center', fontsize=12, color='red')
+#     plt.title(f"Anos: {}")
+#     plt.savefig("meu_grafo.png", format="PNG", dpi=300, bbox_inches="tight")
+#     plt.close()
+#     # time.sleep(5)
+#     input("ENTER para continuar")
--- vias/vias.py
+++ vias/vias.py
@@ -1,43 +1,18 @@
 from funcoes_acessorias import Arquipelago
+from networkx import Graph, draw
+
+
 def main():
     # Input inicial
-    ilhas, n_conexoes, anos = map(int, input("Ilhas, n_conexoes, anos: ").split(" "))
+    n_conexoes, anos = map(int, input("Número de conexões, anos: ").split(" "))
 
-    arquipelago = Arquipelago(ilhas, n_conexoes, anos)
+    arquipelago = Arquipelago(n_conexoes, anos)
     # carrega dados do arquipelago
     arquipelago.pega_conexoes()
     if arquipelago.era_simples():
         print("S")
     else:
         print("N")
-
-
-"""
-def navega_vertices(grafo: dict, anos: int) -> bool:
-    Busca um vértice ainda não visitado e então navega pelos vizinhos atrás de loops.
-    Caso haja um loop de hidrovia, ou mais loops de rodovia do que K anos, retorna `False`.
-    Senão, retorna `True`.
-
-    vertices_visitados: list = []
-    contador: int = 0  
-
-    while ponta:=busca_ponta(grafo, vertices_visitados):
-        vertices_para_visitar = [ponta]
-        while vertices_para_visitar:
-            if contador > anos:
-                return False
-            posicao_atual = vertices_para_visitar.pop(0)
-            vertices_visitados.append(posicao_atual)
-
-            for vizinho in grafo[posicao_atual]:
-                if vizinho in vertices_visitados:
-                    if grafo[posicao_atual][vizinho] == 1:
-                        return False
-                    contador+=1
-                else:
-                    vertices_para_visitar.append(vizinho)
-
-    return True
-"""
+    
 if __name__ == "__main__":
     main()
